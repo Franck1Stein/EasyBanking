35c7a1dcb1ae7f20660e21ee14b27c677da7a6df
franck1Stein <franck.adokou@gmail.com>
2021-08-04 14:34:52 +0000

    overlay completed successfully

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,20 @@
 
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function() {
 
     console.log('open hamburger');
-    if (header.classList.contains('open')) {
+
+    if (header.classList.contains('open')) { // Close Hamburger Menu
         header.classList.remove('open');
-    } else {
+        overlay.classList.add('fade-out');
+        overlay.classList.remove('fade-in');
+    } else { // Open Hamburger Menu
         header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 
 });
